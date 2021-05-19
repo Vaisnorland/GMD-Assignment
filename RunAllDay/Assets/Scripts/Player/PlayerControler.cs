33d5d7f22c97bdbe291ab513f5254bbab38d8a4b
Vaisnorland <lukasvaisnoras@gmail.com>
2021-05-19 11:46:57 +0300

    Full Game

---
+++ RunAllDay/Assets/Scripts/Player/PlayerControler.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+    public float maxSpeed;
+
+    private int desiredLane = 1;
+    public float laneDistance = 4;
+
+    public float jumpForce;
+    public float gravity = -20;
+
+    public Animator animator;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PlayerManager.isGameStarted)
+            return;
+        //increase speed
+        if(forwardSpeed < maxSpeed)
+            forwardSpeed += 0.1f * Time.deltaTime;
+        //runing animation
+        animator.SetBool("isGameStarted",true);
+        direction.z = forwardSpeed;
+        direction.y += gravity * Time.deltaTime;
+
+        if (direction.y > -1)
+        animator.SetBool("isGrounded", controller.isGrounded);
+        else
+        animator.SetBool("isGrounded", true);
+
+
+        if (controller.isGrounded)
+        {
+            if (SwipeManager.swipeUp)
+            {
+
+                Jump();
+            }
+        }
+        if (SwipeManager.swipeRight)
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+            {
+                desiredLane = 2;
+            }
+        }
+ 
+        if (SwipeManager.swipeLeft)
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if(desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        //fixes player controlls
+        if (transform.position == targetPosition)
+            return;
+        Vector3 diff = targetPosition - transform.position;
+        Vector3 moveDir = diff.normalized * 25 * Time.deltaTime;
+        if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+            controller.Move(moveDir);
+        else
+            controller.Move(diff);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!PlayerManager.isGameStarted)
+            return;
+
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.tag == "Obsticle")
+        {
+            PlayerManager.gameOver = true;
+            FindObjectOfType<AudioManager>().PlaySound("GameOver");
+        }
+    }
+}
---
+++ RunAllDay/Assets/Scripts/TileManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0.0f;
+    public float titleLength = 30.0f;
+    public int numberOfTiles = 5;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTrasform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =0; i < numberOfTiles; i++)
+        {
+            if (i == 0)
+                SpawnTile(0);
+            else
+                SpawnTile(Random.Range(1, tilePrefabs.Length));
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTrasform.position.z - 35 > zSpawn - (numberOfTiles * titleLength))
+        {
+            SpawnTile(Random.Range(1, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go);
+        zSpawn += titleLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
